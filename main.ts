51ab637079648551ac7183940bfe457de7d946e2
rokus23 <88277064+rokus23@users.noreply.github.com>
2021-08-01 19:28:36 +0200

    Hangok a számok között, vesztés hang és kiírás

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     if (start == 1) {
         eredmeny = control.millis() - startMillis
+    } else {
+        music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+        basic.showString("early")
+        control.reset()
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -17,8 +21,11 @@ for (let index = 0; index <= 4; index++) {
     while (0 > control.millis() - (index + 1) * 1000) {
     	
     }
+    startMillis = control.millis()
+    music.playTone(294, music.beat(BeatFraction.Sixteenth))
     basic.showNumber(5 - index)
 }
+music.playTone(587, music.beat(BeatFraction.Whole))
 startMillis = control.millis()
 start = 1
 basic.showNumber(0)
